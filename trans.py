0c38175041a919ced05abe1323979688f89e9e3a
knutsun <csselph@gmail.com>
2018-07-16 19:10:11 -0600

    Google Cloud API setup, apiKey, trans request

---
+++ trans.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+
+from google.cloud import translate
+
+def translateText(text, target='en'):
+	translateClient = translate.Client()
+	output = translateClient.translate(text, target_language=target)
+
+	print('Text: ', output['input'])
+	print('Translation: ', output['translatedText'])
+	# print('Detected source language: ', output['detectedSourceLanguage'])
+
+
+inputText = input("What to translate?: ")
+outputText = translateText(inputText)
+print(outputText)
